2e487a4fc9461b724252c25cf2e17ab552e69bbd
Roman Trapeznikov <roma190505@yandex.ru>
2020-01-11 21:09:49 +0300

    мультиплеер почти не работает, удаляем

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import pygame
 import sys
-from game_object import Wall, Ground, Camera, GameObject, LocalPlayer
+from game_object import LocalPlayer, Wall, Ground, Camera, GameObject
 from main_menu import show_main_menu
-from multiplayer import LocalRemotePlayer, RemotePlayer
 import images
-import socket
 
 # Инициализация...
 pygame.init()
@@ -13,7 +11,8 @@ SIZE = WIDTH, HEIGHT = 800, 600
 clock = pygame.time.Clock()
 display = pygame.display.set_mode(SIZE)
 
-# show_main_menu(display)  # TODO: remove on release
+if '-t' not in sys.argv:
+    show_main_menu(display)
 
 # Инициализация мира...
 all_sprites = pygame.sprite.Group()
@@ -31,60 +30,20 @@ look_at.rect = pygame.Rect(0, 0, 0, 0)
 GameObject.init(all_sprites, walls, grounds, entities, players, camera, center)
 images.init()
 
-# player = LocalPlayer(
-#     (all_sprites, players, entities),
-#     (80, -80),
-#     "Первый игрок",
-#     (0, 0),
-#     (pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_f)
-# )
-# another_player = LocalPlayer(
-#     (all_sprites, players, entities),
-#     (80, -80),
-#     "Второй игрок",
-#     (0, 0),
-#     (pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_RCTRL)
-# )
-player = None
-another_player = None
-if '--server' in sys.argv:
-    sock = socket.socket()
-    sock.bind(('0.0.0.0', 1233))
-    sock.listen(1)
-    connection, addr = sock.accept()
-    player = LocalRemotePlayer(
-        (all_sprites,),
-        (0, 0),
-        'Local Player',
-        (0, 0),
-        (pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_f),
-        connection
-    )
-    another_player = RemotePlayer(
-        (all_sprites,),
-        (0, 0),
-        'Remote Player',
-        (0, 0),
-        connection
-    )
-else:
-    connection = socket.socket()
-    connection.connect(('localhost', 1233))
-    player = RemotePlayer(
-        (all_sprites,),
-        (0, 0),
-        'Remote Player',
-        (0, 0),
-        connection
-    )
-    another_player = LocalRemotePlayer(
-        (all_sprites,),
-        (0, 0),
-        'Local Player',
-        (0, 0),
-        (pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_f),
-        connection
-    )
+player = LocalPlayer(
+    (all_sprites, players, entities),
+    (80, -80),
+    "Первый игрок",
+    (0, 0),
+    (pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_f)
+)
+another_player = LocalPlayer(
+    (all_sprites, players, entities),
+    (80, -80),
+    "Второй игрок",
+    (0, 0),
+    (pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_RCTRL)
+)
 blocks = []
 for i in range(100):
     blocks.append(Ground(
@@ -114,9 +73,9 @@ while _running:
     all_sprites.draw(display)
     all_sprites.update()
 
-    w = player.rect.x  # - another_player.rect.x
-    h = player.rect.y  # - another_player.rect.y
-    look_at.rect = pygame.Rect(player.rect.x, player.rect.y, w, h)
+    w = player.rect.x - another_player.rect.x
+    h = player.rect.y - another_player.rect.y
+    look_at.rect = pygame.Rect(another_player.rect.x, another_player.rect.y, w, h)
     camera.update(look_at)
     camera.apply_all(all_sprites.sprites())
 
